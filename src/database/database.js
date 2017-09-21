cf9f81e5fcd22ce7c332090bbbc182b220acddd1
Amrani Houssain <amranidev@gmail.com>
2017-09-21 19:19:24 +0100

    init database

---
+++ src/database/database.js
@@ -0,0 +1,16 @@
+export default class Database {
+    /**
+     * New Database instance
+     * @param {object} store 
+     */
+    constructor(store) {
+        this.database = store.state.database;
+    }
+
+    /**
+     * Get firebase database root ref.
+     */
+    get() {
+        return this.database
+    }
+}
