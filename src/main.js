0c96600dc089a5b060086137371b29335eda94e5
Amrani Houssain <amranidev@gmail.com>
2017-09-21 19:19:37 +0100

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Auth from './auth/auth.js'
 import Storage from './storage/storage.js'
+import Database from './database/database.js'
 
 // late binding
 let Vue
@@ -10,14 +11,26 @@ let mixin = {
     }
 }
 
+let auth
+
+let storage
+
+let database
+
 const Myfirebase = {
 
     install(Vue, options) {
         /**
          * Injetct store and router via Myfirebase options.
          */
-        const { store, router } = options
+        const { store, router, middlewares } = options
+
         Vue.mixin(mixin)
+
+        var VueStore = options.store
+        auth = new Auth(VueStore, options.router)
+        storage = new Storage(VueStore)
+        database = new Database(VueStore)
 
         /**
          * Initialize Firebase Auth global instance.
@@ -25,8 +38,25 @@ const Myfirebase = {
          * @var $auth
          * @var $storage
          */
-        Vue.prototype.$auth = new Auth(options.store, options.router)
-        Vue.prototype.$storage = new Storage(options.store)
+        Vue.prototype.$auth = auth
+        Vue.prototype.$storage = storage
+        Vue.prototype.$database = database
+        Vue.auth = auth
+        Vue.database = database
+        Vue.store = VueStore
+
+        /**
+         * Global Guards middleware.
+         */
+        router.beforeEach((to, from, next) => {
+            var myfirebase = { auth, storage, VueStore }
+            var actions = { to, from, next }
+
+            // register global middlewares
+            options.middlewares.forEach((call) => {
+                call(myfirebase, actions)
+            })
+        })
     }
 }
 
