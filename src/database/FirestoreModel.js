a6d886d2542aaf353c1db9a4ddae64f572dd3814
Houssain Amrani <amranidev@gmail.com>
2018-03-12 21:35:40 +0000

    create FirestoreModel

---
+++ src/database/FirestoreModel.js
@@ -0,0 +1,53 @@
+class FirestoreModel {
+
+    /**
+     * Create new DatabaseModel instance.
+     * 
+     * @constructor
+     * 
+     * @param {*} ref 
+     */
+    constructor(ref) {
+        this.ref = ref
+    }
+
+    /**
+     * Add new document to a collection.
+     * 
+     * @return Promise
+     */
+    add() {
+        this.defineProperties()
+        this.validate(this.props)
+        
+        return this.ref.add(this.props)
+    }
+
+    /**
+     * Remove a document.
+     * 
+     * @param {string} key 
+     * 
+     * @return Promise
+     */
+    delete(key) {
+        this.ensureKey(key)
+        
+        return this.ref.doc(key).delete()
+    }
+
+    /**
+     * Update a document in a collection.
+     * 
+     * @param {string} key
+     * 
+     * @return Promise 
+     */
+    update(key) {
+        this.ensureKey(key)
+
+        return this.ref.doc(key).update(this.props)
+    }
+}
+
+export default FirestoreModel
