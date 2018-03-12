b68d64d89d1e657f13b72daaf79a44d84c426280
Houssain Amrani <amranidev@gmail.com>
2018-03-12 21:07:17 +0000

    create AbstractModel.js

---
+++ src/database/AbstractModel.js
@@ -0,0 +1,56 @@
+class AbstractModel {
+
+    /**
+     * @constructor
+     */
+    constructor() { }
+
+    /**
+     * Define subclass propertires.
+     * 
+     * @return void
+     */
+    defineProperties() {
+        let keys = Object.keys(this)
+        let values = Object.values(this)
+        keys.splice(0, 1)
+        values.splice(0, 1)
+        let props = {}
+        for (let i = 0; i < keys.length; i++) {
+            props[keys[i]] = values[i]
+        }
+        this.props = props
+    }
+
+    /**
+     * Ensure document key.
+     * 
+     * @param {string} key 
+     */
+    ensureKey(key) {
+        if (key === undefined) {
+            throw new Error('Key is undefined')
+        }
+    }
+
+    /**
+     * Validate properties.
+     * 
+     * @var {*} props
+     * 
+     * @return void
+     */
+    validate(props) {
+        let keys = Object.getOwnPropertyNames(props)
+        this.required().forEach((el) => {
+            let result = keys.find((r) => {
+                return el == r && this.props[r] != ""
+            })
+            if (result == undefined) {
+                throw new Error("Property is required")
+            }
+        })
+    }
+}
+
+export default AbstractModel
