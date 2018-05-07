a86213aecdc599abf824f2164c7bedf9b7302314
Amrani Houssain <houssain@airbit.com>
2018-05-07 02:12:58 +0100

    update firestore

--- src/firestore/firestore.js
+++ src/firestore/firestore.js
@@ -3,14 +3,14 @@ export default class Firestore {
      * @constructor
      * @param {Vuex} Vuex
      */
-    constructor(Vuex) {
-        this.firestore = Vuex
-    }
+  constructor (Vuex) {
+    this.firestore = Vuex
+  }
 
     /**
      * Get firestore
      */
-    get() {
-        return this.firestore
-    }
+  get () {
+    return this.firestore
+  }
 }
