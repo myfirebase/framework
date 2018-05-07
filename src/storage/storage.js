85e76e630ad53299224726a51a776dc92ad2b419
Amrani Houssain <houssain@airbit.com>
2018-05-07 02:12:43 +0100

    update storage

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -4,55 +4,55 @@ export default class Storage {
      * @constructor
      * @param {object} store
      */
-    constructor(store) {
-        this.store = store
-    }
+  constructor (store) {
+    this.store = store
+  }
 
     /**
      * retrieve storage object.
      */
-    get() {
-        return this.store.state.storage.storage
-    }
+  get () {
+    return this.store.state.storage.storage
+  }
 
     /**
      * upload a file.
      *
      * @param {object} file
      */
-    upload(file) {
-        this.store.commit('storage/uploadFile', file)
-    }
+  upload (file) {
+    this.store.commit('storage/uploadFile', file)
+  }
 
     /**
      * download a file.
-     * 
+     *
      * @param {object} reference
      */
-    getDownloadURL(file) {
-        let promis = this.get().ref().child(file.ref).getDownloadURL()
-        promis.then(url => {
-            file.result(url)
-        }).catch(error => {
-            file.error(error)
-        })
-    }
+  getDownloadURL (file) {
+    let promis = this.get().ref().child(file.ref).getDownloadURL()
+    promis.then(url => {
+      file.result(url)
+    }).catch(error => {
+      file.error(error)
+    })
+  }
 
     /**
      * delete file.
      *
      * @param {object} file
      */
-    delete(file) {
-        this.store.commit('storage/deleteFile', file)
-    }
+  delete (file) {
+    this.store.commit('storage/deleteFile', file)
+  }
 
     /**
      * upload multiple files.
-     * 
+     *
      * @param {object} files
      */
-    uploadMultiple(files) {
-        this.store.commit('storage/uploadFiles', files)
-    }
+  uploadMultiple (files) {
+    this.store.commit('storage/uploadFiles', files)
+  }
 }
